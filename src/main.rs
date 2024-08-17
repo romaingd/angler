a72e659436fb4d6b8a7f3a046564e31b1f3970b4
Romain Girard <34568327+romaingd@users.noreply.github.com>
2024-08-17 16:18:36 +0200

    write hook

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::OpenOptions,
+    io::Write,
+    os::unix::fs::{OpenOptionsExt, PermissionsExt},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let rg_cmd = "rg -e 'fail-pat' ~/code/angler/";
+    let hook_path = "/Users/romain/code/angler/.git/hooks/pre-commit";
+
+    let mut options = OpenOptions::new();
+    let mut file = options
+        .write(true)
+        .create(true)
+        .mode(0o777)
+        .open(hook_path)
+        .expect("Could not open file");
+
+    let mut perms = file.metadata()?.permissions();
+    perms.set_mode(0o777);
+    file.set_permissions(perms)?;
+
+    file.write_all(rg_cmd.as_bytes())
+        .expect("Could not write to file");
+
+    Ok(())
 }
